6f5939f72dadd2ce9f95daea09c5423f242f8602
Michael Meir <michael.meir@notive.nl>
2022-03-08 20:46:04 +0100

    added warning logging

--- internal/commands/say.go
+++ internal/commands/say.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"github.com/bwmarrin/discordgo"
+	"github.com/meir/discord-bot/internal/logging"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -23,5 +24,8 @@ func say(session *discordgo.Session, interaction *discordgo.InteractionCreate, d
 
 	session.ChannelMessageSend(interaction.ChannelID, interaction.ApplicationCommandData().Options[0].StringValue())
 
-	session.InteractionResponseDelete(session.State.User.ID, interaction.Interaction)
+	err := session.InteractionResponseDelete(session.State.User.ID, interaction.Interaction)
+	if err != nil {
+		logging.Warn(err)
+	}
 }

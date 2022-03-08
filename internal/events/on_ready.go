f243d0ec8b3fe0f470d5f2f161c4b53709571ac3
Michael Meir <michael.meir@notive.nl>
2022-03-08 13:32:32 +0100

    update

--- internal/events/on_ready.go
+++ internal/events/on_ready.go
@@ -9,16 +9,15 @@ import (
 )
 
 func OnReady(session *discordgo.Session, ready *discordgo.Event) {
-	err := session.UpdateGameStatus(0, fmt.Sprintf("Running version %v", os.Getenv("VERSION")))
-	// activity := &discordgo.Activity{
-	// 	Name: fmt.Sprintf("Running version %v", os.Getenv("VERSION")),
-	// 	Type: discordgo.ActivityTypeCustom,
-	// }
+	activity := &discordgo.Activity{
+		Name: fmt.Sprintf("Running version %v", os.Getenv("VERSION")),
+		Type: discordgo.ActivityTypeCustom,
+	}
 
-	// err := session.UpdateStatusComplex(discordgo.UpdateStatusData{
-	// 	Activities: []*discordgo.Activity{activity},
-	// 	Status:     string(discordgo.StatusOnline),
-	// })
+	err := session.UpdateStatusComplex(discordgo.UpdateStatusData{
+		Activities: []*discordgo.Activity{activity},
+		Status:     string(discordgo.StatusOnline),
+	})
 	if err != nil {
 		logging.Warn("Failed to update status", err)
 	}

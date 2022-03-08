7f57c2b51b7f2c4c59aa390c268d6cd26fb080e9
Michael Meir <michael.meir@notive.nl>
2022-03-08 12:59:14 +0100

    changed status code

--- internal/events/on_ready.go
+++ internal/events/on_ready.go
@@ -5,8 +5,20 @@ import (
 	"os"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/meir/discord-bot/internal/logging"
 )
 
 func OnReady(session *discordgo.Session, ready *discordgo.Ready) {
-	session.UpdateGameStatus(0, fmt.Sprintf("version #%v", os.Getenv("VERSION")))
+	activity := &discordgo.Activity{
+		Name: fmt.Sprintf("Running version %v", os.Getenv("VERSION")),
+		Type: discordgo.ActivityTypeCustom,
+	}
+
+	err := session.UpdateStatusComplex(discordgo.UpdateStatusData{
+		Activities: []*discordgo.Activity{activity},
+		Status:     string(discordgo.StatusOnline),
+	})
+	if err != nil {
+		logging.Warn("Failed to update status", err)
+	}
 }

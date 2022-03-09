28d38340e66001ae6006b49f58c4092099af6053
Michael Meir <michael.meir@notive.nl>
2022-03-09 15:05:01 +0100

    removed errors package

--- internal/logging/logging.go
+++ internal/logging/logging.go
@@ -6,10 +6,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"strings"
 	"time"
-
-	"github.com/go-errors/errors"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/fatih/color"
@@ -53,21 +50,12 @@ func webhook(t string, l ...interface{}) {
 		return
 	}
 
-	traces := []string{}
-	for _, v := range l {
-		if err, ok := v.(error); ok {
-			trace := err.(*errors.Error).ErrorStack()
-			traces = append(traces, trace)
-			fmt.Println(trace)
-		}
-	}
-
 	wh := discordgo.WebhookParams{
 		Embeds: []*discordgo.MessageEmbed{
 			{
 				Title:       t,
 				Color:       typeColor[t],
-				Description: fmt.Sprintf("> %v\n%v", fmt.Sprintln(l...), strings.Join(traces, "\n---\n")),
+				Description: fmt.Sprintf("> %v", fmt.Sprintln(l...)),
 				Timestamp:   time.Now().Format("2006-01-02 15:04"),
 				Footer: &discordgo.MessageEmbedFooter{
 					Text: fmt.Sprintf("Build Version VH-%s", os.Getenv("VERSION")),

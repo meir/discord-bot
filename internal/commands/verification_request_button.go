39b0b4b9e272a5c8d699e0c45cda370491850927
Michael Meir <michael.meir@notive.nl>
2022-03-10 07:40:07 +0100

    fixed verification request button hopefully

--- internal/commands/verification_request_button.go
+++ internal/commands/verification_request_button.go
@@ -14,6 +14,27 @@ func init() {
 	appendComponent(discordgo.ButtonComponent, "verification_request_button", verification_request_button)
 }
 
+/**
+
+permissions
+
+db.getchannel X
+
+if db.channel = exists {
+	if dsc.channel != exists {
+		dsc.channel.create
+		db.channel.update
+		response
+		return
+	}
+	response
+	return
+}
+db.channel.create
+dsc.channel.create
+response
+**/
+
 func verification_request_button(session *discordgo.Session, interaction *discordgo.InteractionCreate, db *mongo.Database) {
 	query := structs.NewQuery(session, interaction, db)
 
@@ -44,87 +65,94 @@ func verification_request_button(session *discordgo.Session, interaction *discor
 	channel, err := query.ChannelByFilter(bson.M{
 		"guild_id": interaction.GuildID,
 		"metadata": bson.M{
-			"type": string(structs.VERIFICATION_CHANNEL),
+			"type": structs.VERIFICATION_CHANNEL,
 			"user": interaction.Member.User.ID,
 		},
 	})
-	if err == mongo.ErrNoDocuments {
-		var ch *discordgo.Channel
-		ch, err = session.GuildChannelCreateComplex(interaction.GuildID, discordgo.GuildChannelCreateData{
-			Name:                 fmt.Sprintf("verification-%v", interaction.Member.User.ID),
-			Type:                 discordgo.ChannelTypeGuildText,
-			Topic:                "You can explain here why you should be allowed into the server! Good luck :)",
-			PermissionOverwrites: overwrites,
-			ParentID:             guild.VerificationMessage.ParentID,
-		})
+	if err != nil && err != mongo.ErrNoDocuments {
+		logging.Warn(err)
+		return
+	}
+
+	if err != mongo.ErrNoDocuments {
+		channels, err := session.GuildChannels(interaction.GuildID)
 		if err != nil {
-			logging.Warn("failed to create channel", err)
+			logging.Warn(err)
 			return
 		}
-		channel = query.NewChannel(interaction.GuildID, ch.ID)
-		channel.Metadata = map[string]string{
-			"type": string(structs.VERIFICATION_CHANNEL),
-			"user": interaction.Member.User.ID,
+		exists := false
+		for _, v := range channels {
+			if v.ID == channel.ChannelID {
+				exists = true
+			}
 		}
-		err = channel.Update()
+		if !exists {
+			ch, err := session.GuildChannelCreateComplex(interaction.GuildID, discordgo.GuildChannelCreateData{
+				Name:                 fmt.Sprintf("verification-%v", interaction.Member.User.ID),
+				Type:                 discordgo.ChannelTypeGuildText,
+				Topic:                "You can explain here why you should be allowed into the server! Good luck :)",
+				PermissionOverwrites: overwrites,
+				ParentID:             guild.VerificationMessage.ParentID,
+			})
+			if err != nil {
+				logging.Warn("failed to create channel", err)
+				return
+			}
+			channel.ChannelID = ch.ID
+			err = channel.Update()
+			if err != nil {
+				logging.Warn("failed to create channel", err)
+				return
+			}
+
+			session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Flags:   1 << 6,
+					Content: fmt.Sprintf("Your verification channel has been created: %v", ch.Mention()),
+				},
+			})
+			return
+		}
+
 		session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
 			Type: discordgo.InteractionResponseChannelMessageWithSource,
 			Data: &discordgo.InteractionResponseData{
 				Flags:   1 << 6,
-				Content: fmt.Sprintf("Your verification channel has been created: %v", ch.Mention()),
+				Content: fmt.Sprintf("You already have an open verification channel: <@%v>", channel.Metadata["user"]),
 			},
 		})
 		return
 	}
+
+	ch, err := session.GuildChannelCreateComplex(interaction.GuildID, discordgo.GuildChannelCreateData{
+		Name:                 fmt.Sprintf("verification-%v", interaction.Member.User.ID),
+		Type:                 discordgo.ChannelTypeGuildText,
+		Topic:                "You can explain here why you should be allowed into the server! Good luck :)",
+		PermissionOverwrites: overwrites,
+		ParentID:             guild.VerificationMessage.ParentID,
+	})
 	if err != nil {
-		logging.Warn("Error after trying to find channel or update", err)
+		logging.Warn("failed to create channel", err)
 		return
 	}
 
-	channelList, err := session.GuildChannels(interaction.GuildID)
+	channel = query.NewChannel(interaction.GuildID, ch.ID)
+	channel.Metadata = map[string]string{
+		"type": string(structs.VERIFICATION_CHANNEL),
+		"user": interaction.Member.User.ID,
+	}
+	err = channel.Update()
 	if err != nil {
 		logging.Warn(err)
 		return
 	}
-	exists := false
-	for _, v := range channelList {
-		if v.ID == channel.ChannelID {
-			exists = true
-		}
-	}
 
-	if !exists {
-		ch, err := session.GuildChannelCreateComplex(interaction.GuildID, discordgo.GuildChannelCreateData{
-			Name:                 fmt.Sprintf("verification-%v", interaction.Member.User.ID),
-			Type:                 discordgo.ChannelTypeGuildText,
-			Topic:                "You can explain here why you should be allowed into the server! Good luck :)",
-			PermissionOverwrites: overwrites,
-			ParentID:             guild.VerificationMessage.ParentID,
-		})
-		if err != nil {
-			logging.Warn("failed while creating channel from model", err)
-			return
-		}
-		channel.ChannelID = ch.ID
-		err = channel.Update()
-		if err != nil {
-			logging.Warn("failed to update channel model after channel fallback", err)
-			return
-		}
-		session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Flags:   1 << 6,
-				Content: fmt.Sprintf("Your verification channel has been created: %v", ch.Mention()),
-			},
-		})
-		return
-	}
 	session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: &discordgo.InteractionResponseData{
 			Flags:   1 << 6,
-			Content: fmt.Sprintf("You already have an open verification channel: <@%v>", channel.Metadata["user"]),
+			Content: fmt.Sprintf("Your verification channel has been created: %v", ch.Mention()),
 		},
 	})
 }
